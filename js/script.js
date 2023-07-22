420967a65189e5beb8eec950aee6e4fb589a49fa
Diogo Henrique <diogohs110@gmail.com>
2023-07-22 09:27:47 -0300

    Add remove button and change status button

--- js/script.js
+++ js/script.js
@@ -15,65 +15,104 @@ function createNewBook(e) {
 	);
 
 	addBookToLibrary(newBook);
-	displayAllBooks();
+	displayNewBook();
+	form.reset();
 }
 
 function addBookToLibrary(book) {
 	myLibrary.push(book);
 }
 
-function displayAllBooks() {
-	clearBookContainer();
-	for (let i = 0; i < myLibrary.length; i++) {
-		booksContainer.appendChild(createBookElement(myLibrary[i]));
-	}
+function displayNewBook() {
+	booksContainer.appendChild(createBookCard(myLibrary[myLibrary.length - 1]));
 }
 
-function clearBookContainer() {
-	booksContainer.innerHTML = '';
+function createBookCard(book) {
+	// Card Front
+	const cardFront = createElement('div', 'card__front', '');
+
+	const cardTitle = createElement('h2', 'card__title', book.title);
+	const cardAuthor = createElement('p', 'card__author', book.author);
+	const cardPages = createElement('p', 'card__pages',  book.pages);
+	const cardStatus = createElement('p', 'card__status', book.readStatus ? 'read' : 'not read');
+
+	cardFront.appendChild(cardTitle);
+	cardFront.appendChild(cardAuthor);
+	cardFront.appendChild(cardPages);
+	cardFront.appendChild(cardStatus);
+
+	// Card Back
+	const cardBack = createElement('div', 'card__back', '');
+
+	const removeButton = createElement('button', 'card__remove', '');
+	const removeButtonIcon = createElement('i', ['fa-regular', 'fa-trash-can'],'');
+	const changeStatusButton = createElement('button', 'card__change-status', 'Change status');
+
+	changeStatusButton.addEventListener('click', (e) => {changeReadStatus(e)})
+	removeButton.addEventListener('click', (e) => {removeBook(e);});
+
+	removeButton.appendChild(removeButtonIcon);
+	cardBack.appendChild(removeButton);
+	cardBack.appendChild(changeStatusButton);
+
+	// Card Wrapper
+	const card = createElement('div', 'card', '');
+
+	card.appendChild(cardFront);
+	card.appendChild(cardBack);
+
+	card.dataset.id = myLibrary.length - 1;
+	card.dataset.read = book.readStatus;
+
+	return card;
 }
 
-function createBookElement(book) {
-	const bookElement = createElement('div', 'book', '');
-	const bookTitle = createElement('h2', 'book__title', book.title);
-	const bookAuthor = createElement('p', 'book__author', book.author);
-	const bookPages = createElement('p', 'book__pages', book.pages);
-	const bookStatus = createElement('p', 'book__status', book.wasRead ? 'already read!' : 'not read yet!');
-
-	bookElement.appendChild(bookTitle);
-	bookElement.appendChild(bookAuthor);
-	bookElement.appendChild(bookPages);
-	bookElement.appendChild(bookStatus);
-
-	return bookElement;
-}
-
-function createElement(tag, className, content) {
+function createElement(tag, classes, content) {
 	const newElement = document.createElement(tag);
-	newElement.classList.add(className);
 	newElement.textContent = content;
+	if (typeof classes === 'string') {
+		newElement.classList.add(classes);
+	} else {
+		newElement.classList.add(...classes);
+	}
 
 	return newElement;
 }
 
-function Book(title, author, pages, wasRead) {
+function removeBook(e) {
+	const clickedBook = e.target.closest('.card');
+	myLibrary.splice(clickedBook.dataset.id, 1);
+	booksContainer.removeChild(clickedBook);
+}
+
+function changeReadStatus(e) {
+	const clickedBook = e.target.closest('.card');
+	myLibrary[clickedBook.dataset.id].changeReadStatus();
+	updateCard(clickedBook);
+}
+
+function updateCard(card) {
+	if (myLibrary[card.dataset.id].readStatus) {
+		card.querySelector('.card__status').textContent= 'read';
+	} else {
+		card.querySelector('.card__status').textContent = 'not read';
+	}
+}
+
+function Book(title, author, pages, readStatus) {
 	this.title = title;
 	this.author = author;
 	this.pages = pages;
-	this.wasRead = wasRead;
+	this.readStatus = readStatus;
 }
 
+Book.prototype.changeReadStatus = function() {
+	this.readStatus = !this.readStatus;
+}
 
 // Open Form Button
-
 const openBtn = document.querySelector('.library__control button');
 
 openBtn.addEventListener('click', (e) => {
 	document.querySelector('.library__header').classList.toggle('open');
-})
-
-
-const addButton = document.querySelector('.library__form button')
-
-addButton.click();
-addButton.click();
+});
